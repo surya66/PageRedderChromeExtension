b6a7b3882b1dcab16e41166bce54b0a1e5b6e39c
Surya Mettupalli <surya66@gmail.com>
2016-10-05 10:14:07 -0400

    Add files via upload

---
+++ background.js
@@ -0,0 +1,28 @@
+// Background page -- background.js
+//alert("background.js");
+
+chrome.runtime.onConnect.addListener(function(devToolsConnection) {
+  //alert("inside background.js -- onConnect");
+    // assign the listener function to a variable so we can remove it later
+    var devToolsListener = function(message, sender, sendResponse) {
+      //alert("injecting Content Script");
+        // Inject a content script into the identified tab
+        if (message.name == "init") {
+            
+          //alert("injecting Content Script - inside init");
+            //alert("tab ID " + message.tabId) ;
+            //alert("message.scriptToInject: " + message.scriptToInject) ;
+            
+            
+            chrome.tabs.executeScript(message.tabId,
+            {file: message.scriptToInject });
+        }
+
+    }
+    // add the listener
+    devToolsConnection.onMessage.addListener(devToolsListener);
+
+    devToolsConnection.onDisconnect.addListener(function() {
+         devToolsConnection.onMessage.removeListener(devToolsListener);
+    });
+})
---
+++ devtools.js
@@ -0,0 +1,6 @@
+chrome.devtools.panels.elements.createSidebarPane("Redder",
+    function(sidebar) {
+         sidebar.setPage("panel.html");
+         
+    }
+);
